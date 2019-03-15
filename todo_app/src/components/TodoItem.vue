37c35795507ad90670aee20cd2c0b6740121dcd9
Alexis Michaltsis <alexis@michaltsis.net>
2019-03-15 17:13:29 +0100

    Mark and delete todos

--- todo_app/src/components/TodoItem.vue
+++ todo_app/src/components/TodoItem.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    <p>{{todo.title}}</p>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete">
+      {{todo.title}}
+      <button @click="$emit('delete-todo', todo.id)" class="delete">x</button>
+      </p>
   </div>
 </template>
 
 <script>
 export default {
   name: "TodoItem",
-  props: ["todo"]
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed
+    }
+  }
 }
 </script>
 
@@ -20,7 +29,7 @@ export default {
   .is-complete {
     text-decoration: line-through;
   }
-  .del {
+  .delete {
     background: #ff0000;
     color: #fff;
     border: none;
