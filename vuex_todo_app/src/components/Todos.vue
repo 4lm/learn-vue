ff192bdc0c6ea566c2711dbfce917a92ca461c7d
Alexis Michaltsis <alexis@michaltsis.net>
2019-03-17 21:12:48 +0100

    Add updateTodo

--- vuex_todo_app/src/components/Todos.vue
+++ vuex_todo_app/src/components/Todos.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
     <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span>
+        <span class="incomplete-box"></span> = Incomplete
+      </span>
+      <span>
+        <span class="complete-box"></span> = Complete
+      </span>
+    </div>
     <div class="todos">
-      <div v-for="todo in allTodos" :key="todo.id" class="todo">{{todo.title}}
+      <div
+      @dblclick="onDblClick(todo)"
+      v-for="todo in allTodos"
+      :key="todo.id"
+      class="todo"
+      v-bind:class="{'is-complete':todo.completed}">
+        {{todo.title}}
         <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
       </div>
     </div>
@@ -15,11 +30,20 @@ import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Todos",
   methods: {
-    ...mapActions(["fetchTodos", "deleteTodo"])
+    ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+    onDblClick(todo) {
+      const updateTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed
+      };
+
+      this.updateTodo(updateTodo);
+    }
   },
   computed: mapGetters(["allTodos"]),
   created() {
-    this.fetchTodos()
+    this.fetchTodos();
   }
 };
 </script>
@@ -46,5 +70,32 @@ i {
   right: 10px;
   color: #fff;
   cursor: pointer;
+}
+
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
