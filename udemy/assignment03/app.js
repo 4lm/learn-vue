e76ac2fe89d39f301b3143d8774bafa9ddca9c46
Alexis Michaltsis <alexis@michaltsis.net>
2019-07-15 12:47:57 +0200

    Add assignment 3

---
+++ udemy/assignment03/app.js
@@ -0,0 +1,21 @@
+new Vue({
+        el: '#exercise',
+        data: {
+            value: 0
+        },
+        computed: {
+            result: function() {
+                return this.value == 37 ? "done" : "not there yet";
+            }
+        },
+        watch: {
+            result: function() {
+                vm = this;
+                setTimeout(() => {
+                    vm.value = 0;
+                },
+                5000
+                );
+            }
+        }
+    });
