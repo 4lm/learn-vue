cb1d528a2ccfee9f044d02d80d79260ff73b30f6
Alexis Michaltsis <alexis@michaltsis.net>
2019-07-15 18:18:25 +0200

    Add attack method

--- udemy/project01/app.js
+++ udemy/project01/app.js
@@ -10,6 +10,37 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function () {
+            var max = 10;
+            var min = 3;
+            var damage = parseInt(Math.random() * (max - min) + min);
+            this.monsterHealth -= damage;
+
+            if (this.monsterHealth <= 0) {
+                alert("You won!");
+                this.gameIsRunning = false;
+                return;
+            }
+
+            max = 12;
+            min = 5;
+            damage = parseInt(Math.random() * (max - min) + min);
+            this.playerHealth -= damage;
+
+            if (this.playerHealth <= 0) {
+                alert("You lost!");
+                this.gameIsRunning = false;
+            }
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+
         }
     }
 });
